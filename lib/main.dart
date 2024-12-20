b72042653ffcc68ede3fddbd16374492fc2e5449
Myatty <myintmyatps664@gmail.com>
2024-12-20 18:07:39 +0630

    Expanded, Flex and button syling

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData.dark(),
+        theme: ThemeData.dark(useMaterial3: true),
         home: Scaffold(
           appBar: AppBar(
             title: Text("Billionaire App"),
@@ -27,9 +27,26 @@ class MyApp extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Balance"),
-                ElevatedButton(
-                    onPressed: addFunction, child: Text("Click here"))
+                Expanded(
+                  flex: 9,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Balance"),
+                      SizedBox(height: 10),
+                      Text("0")
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red[700],
+                          minimumSize: Size(double.infinity, 0)),
+                      onPressed: addFunction,
+                      child: Text("Click here")),
+                )
               ],
             ),
           ),
