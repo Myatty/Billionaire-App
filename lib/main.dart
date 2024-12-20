a49cb2a07a9f96e3f41500c43621d8b669b61713
Myatty <myintmyatps664@gmail.com>
2024-12-20 17:02:26 +0630

    Change to Dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData.dark(),
         home: Scaffold(
-      appBar: AppBar(
-        title: Text("Billionaire App"),
-      ),
-    ));
+          appBar: AppBar(
+            title: Text("Billionaire App"),
+          ),
+        ));
   }
 }
